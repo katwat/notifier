bcbe6f304790ead01de7e13be5e28e4ed7eb9de3
katwat <super.tokachi@gmail.com>
2023-03-02 15:51:42 +0900

    refactor: gulp-***をできるだけインライン化して依存モジュールを減らすように改善。

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,117 @@
-var gulp = require('gulp'); // v4
+const gulp = require('gulp'),
+	stylelint = require('stylelint'),
+	{ ESLint } = require('eslint'),
+	cleancss = require('clean-css'),
+	uglifyjs = require('uglify-js'),
+	replaceExt = require('replace-ext');
 
-gulp.task('lint', function () {
-	var stylelint = require('stylelint'),
-		postcss = require('gulp-postcss'),
-		eslint = require('gulp-eslint');
+function lintCSS() {
+	return gulp.src('src/notifier.css')
+	.on('data',_stylelint);
+}
 
-	gulp.src('src/notifier.css')
-	.pipe(postcss([
-		stylelint()
-	]));
-
+function lintJS() {
 	return gulp.src([
 		'gulpfile.js',
 		'src/notifier.js'
 	])
-	.pipe(eslint({
-		envs: [
-			'node',
-			'browser',
-			'worker',
-			'es6'
-		],
-		rules: {
-			'semi': 1,
-			'no-undef': 1,
-			'no-extra-semi': 1,
-			'no-unused-vars': 1
-		},
-		globals: [
-			'Notifier'
-		]
-	}))
-	.pipe(eslint.format());
-});
+	.on('data',_eslint);
+}
 
-gulp.task('js', function() {
-	var uglify = require('gulp-uglify'),
-		rename = require('gulp-rename');
+function minifyCSS() {
+	return gulp.src('src/notifier.css')
+	.on('data',function(file) {
+		_cleancss(file);
+		file.path = replaceExt(file.path,'.min.css');
+	})
+	.pipe(gulp.dest('build/'));
+}
 
+function minifyJS() {
 	return gulp.src('src/notifier.js')
-	.pipe(uglify({
+	.on('data',function(file) {
+		_uglifyjs(file);
+		file.path = replaceExt(file.path,'.min.js');
+	})
+	.pipe(gulp.dest('build/'));
+}
+
+function _stylelint(file) {
+	stylelint.lint({
+		config: {
+			extends: 'stylelint-config-recommended'
+		},
+		codeFilename: file.path,
+		code: file.contents.toString(),
+		formatter: 'string'
+	})
+	.then(result => {
+		if (result.output) {
+			console.log(result.output);
+		}
+	})
+	.catch(error => {
+		process.exitCode = 1;
+		console.error(error);
+	});
+}
+
+function _eslint(file) {
+	(async function() {
+		// 1. Create an instance
+		const eslint = new ESLint({
+			useEslintrc: false,
+			overrideConfig: {
+				extends: 'eslint:recommended',
+				parserOptions: {
+					//sourceType: 'module',
+					ecmaVersion: 'latest'
+				},
+				env: {
+					node: true,
+					browser: true,
+					worker: true,
+					es2022: true
+				},
+				globals: {
+					Notifier: 'writable'
+				}
+			},
+		});
+
+		// 2. Lint text.
+		const results = await eslint.lintText(file.contents.toString(),{
+			filePath: file.path
+		});
+
+		// 3. Format the results.
+		const formatter = await eslint.loadFormatter('stylish');
+		const resultText = formatter.format(results);
+
+		// 4. Output it.
+		if (resultText) {
+			console.log(resultText);
+		}
+	})().catch(error => {
+		process.exitCode = 1;
+		console.error(error);
+	});
+}
+
+function _cleancss(file) {
+	const result = new cleancss(/* options */).minify(file.contents.toString());
+	file.contents = Buffer.from(result.styles);
+}
+
+function _uglifyjs(file) {
+	const result = uglifyjs.minify(file.contents.toString(),{
 		output: {
-			//quote_style: 1,
 			comments: /^!/
 		}
-	}))
-	.pipe(rename({
-		extname: '.min.js'
-	}))
-	.pipe(gulp.dest('build/'));
-});
+	});
+	file.contents = Buffer.from(result.code);
+}
 
-gulp.task('css', function() {
-	var cleancss = require('gulp-clean-css'), // minify css
-		rename = require('gulp-rename');
+exports.default = gulp.parallel(minifyCSS,minifyJS);
 
-	return gulp.src('src/notifier.css')
-	.pipe(cleancss())
-	.pipe(rename({
-		extname: '.min.css'
-	}))
-	.pipe(gulp.dest('build/'));
-});
-
-gulp.task('default', gulp.parallel('css','js'));
+exports.lint = gulp.parallel(lintCSS,lintJS);
